623aab875fa611ebcd3c3e265f55cf50f2658226
Bote <58780286+dorogus1@users.noreply.github.com>
2022-10-20 11:37:17 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+function changeImage() {
+    if (document.getElementById("imgClickAndChange").src == "uni.jpg"){
+        document.getElementById("imgClickAndChange").src = "labs.jpg";
+    } else {
+        document.getElementById("imgClickAndChange").src = "labs.jpg";
+    }
+}
+
+
